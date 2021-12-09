055d8f28abcbb13cf55470e72c9a15d4f78c802f
Toni <twtiainen@gmail.com>
2021-12-09 22:11:27 +0200

    Fix bottom navigation

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -1,34 +1,55 @@
 <script>
-    import Icon from "@iconify/svelte";
     import {getStores} from "$app/stores";
     const {page} = getStores();
     console.log(page);
-
-
 </script>
 
-<nav class="fixed bottom-0 flex justify-between w-full bg-gray-50 py-2">
-    <a href="/">
+<nav class="fixed bottom-0 flex justify-between w-full bg-gray-50 text-gray-400">
+    <a href="/" class={$page.path === '/' ? 'active':''}>
         <!-- Heroicons 'home' icon -->
+        { #if $page.path === '/'}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+          </svg>
+        { :else }
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+        { /if }
         Home
     </a>
-    <a href="/materials">
+    <a href="/materials" class={$page.path === '/materials' ? 'active':''}>
         <!-- Heroicons 'book' icon -->
+        { #if $page.path === '/materials'}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
+          </svg>
+        { :else }
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+        { /if }
+        
         Materials
     </a>
-    <a href="/assignments">
+    <a href="/assignments" class={$page.path === '/assignments' ? 'active':''}>
         <!-- Heroicons 'collection' icon -->
+        { #if $page.path === '/assignments'}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+          </svg>
+        { :else }
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
+        { /if }
+        
         Assignments
     </a>
-    <a href="/calendar">
-        <!-- Heroicons 'calendar' icon -->
-        <!--
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-        -->
-        <Icon icon="bx:bxs-home" height={24}/>
+    <a href="/calendar" class={$page.path === '/calendar' ? 'active':''}>
+        { #if $page.path === '/calendar'}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+          </svg>
+        { :else }
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          </svg>
+        { /if }
         Calendar
     </a>
 </nav>
@@ -42,5 +63,17 @@
         width: 25%;
         text-align: center;
         font-size: 11px;
+        margin: 4px 12px;
+        padding: 4px;
+        border-radius: 16px;
+        transition: background-color ease 0.2s;
+    }
+
+    nav a.active {
+        color: rgb(37 99 235);
+    }
+
+    nav a:hover {
+        background-color: rgba(37, 99, 235, 0.03);
     }
 </style>
