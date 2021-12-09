1e959aabc8a1424c4fdd28149b4362101276cdef
towatiai <twtiainen@gmail.com>
2021-12-09 18:27:10 +0200

    Add wip bottom navigation and iconify

---
+++ src/components/BottomNav.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Icon from "@iconify/svelte";
+    import {getStores} from "$app/stores";
+    const {page} = getStores();
+    console.log(page);
+
+
+</script>
+
+<nav class="fixed bottom-0 flex justify-between w-full bg-gray-50 py-2">
+    <a href="/">
+        <!-- Heroicons 'home' icon -->
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+        Home
+    </a>
+    <a href="/materials">
+        <!-- Heroicons 'book' icon -->
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+        Materials
+    </a>
+    <a href="/assignments">
+        <!-- Heroicons 'collection' icon -->
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
+        Assignments
+    </a>
+    <a href="/calendar">
+        <!-- Heroicons 'calendar' icon -->
+        <!--
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+        -->
+        <Icon icon="bx:bxs-home" height={24}/>
+        Calendar
+    </a>
+</nav>
+
+<style>
+    nav a {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 25%;
+        text-align: center;
+        font-size: 11px;
+    }
+</style>
